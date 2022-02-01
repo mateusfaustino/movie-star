7e493da423254869bb626cbd7732092a6c78b714
Mateus Faustino <mateusfaustino2017@gmail.com>
2022-02-01 20:54:30 -0300

    update db

--- database/db.sql
+++ database/db.sql
@@ -2,10 +2,35 @@ CREATE DATABASE my_db;
 
 USE my_db;
 
-CREATE TABLE `category2` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) COLLATE utf16_bin NOT NULL,
-  `description` varchar(150) COLLATE utf16_bin,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
+CREATE TABLE users (
+	id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(100),
+	lastname VARCHAR(100),
+	email VARCHAR(200),
+	password VARCHAR(200),
+	image VARCHAR(200),
+	token VARCHAR(200),
+	bio TEXT
+);
+
+CREATE TABLE movie(
+	id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	title VARCHAR(100),
+	description TEXT,
+	image VARCHAR(200),
+	trailer VARCHAR(150),
+	category VARCHAR (50),
+	length VARCHAR(45),
+	users_id INT(11) unsigned,
+	FOREIGN KEY(users_id) REFERENCES users(id)
+);
+
+CREATE TABLE reviews(
+  id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  rating INT,
+  review TEXT,
+  users_id INT(11) UNSIGNED,
+  movies_id INT(11) UNSIGNED,
+  FOREIGN KEY (users_id) REFERENCES users(id),
+  FOREIGN KEY (movies_id) REFERENCES movie(id)
 );
