b499204ca48470cc281774404d5c885caf5e786d
Mateus Faustino <mateusfaustino2017@gmail.com>
2022-02-05 15:09:21 -0300

    conectando ao bd

--- index.php
+++ index.php
@@ -1,30 +1,24 @@
 <?php
-    require_once("globals.php");
-    require_once("db.php");
+  require_once("templates/header.php");
+
+//   require_once("dao/MovieDAO.php");
+
+  // DAO dos filmes
+//   $movieDao = new MovieDAO($conn, $BASE_URL);
+
+//   $latestMovies = $movieDao->getLatestMovies();
+
+//   $actionMovies = $movieDao->getMoviesByCategory("Ação");
+
+//   $comedyMovies = $movieDao->getMoviesByCategory("Comédia");
+
 ?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Movie Star</title>
-    <link rel="short icon" href="img/moviestar.ico">
-    <link rel="stylesheet" href="css/style.css">
-    <!-- Fontawsome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- Boostrap -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.css" integrity="sha512-Ty5JVU2Gi9x9IdqyHN0ykhPakXQuXgGY5ZzmhgZJapf3CpmQgbuhGxmI4tsc8YaXM+kibfrZ+CNX4fur14XNRg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-</head>
-<body>
-    <h1>
-        Testando
-        <?php
-            echo $BASE_URL ;
-        ?>
-    </h1>
-    <!-- Bootstrap JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/js/bootstrap.js" integrity="sha512-0agUJrbt+sO/RcBuV4fyg3MGYU4ajwq2UJNEx6bX8LJW6/keJfuX+LVarFKfWSMx0m77ZyA0NtDAkHfFMcnPpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-</body>
-</html>
+<h1>
+    Testando
+<?php
+    echo $BASE_URL ;
+?>
+</h1>
+<?php
+    require_once("templates/footer.php");
+?>
